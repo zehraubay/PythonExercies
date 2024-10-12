69c422b188a75067b21cdafa6375fdad25e77fac
Zehra Ubay <zehraubay276@gmail.com>
2024-10-12 22:59:59 +0300

    Dongulere baslandi

--- functions_conditions_loops_comprehesions.py
+++ functions_conditions_loops_comprehesions.py
@@ -226,3 +226,157 @@ def fahrenheit_to_kelvin(f):
     return kelvin
 
 print(fahrenheit_to_kelvin(98.6))  # 310.15
+
+
+
+
+
+# Koşullar (Conditions)
+
+# True - False
+1 == 1
+1 == 2
+1 != 2
+
+#if 
+
+if 1 == 1:
+    print("Doğru")
+
+if 1 == 2:
+    print("Yanlış") # if sadece sonucu True olan işlemi yapar
+
+number = 11
+if number == 10:
+    print("number is 10") # if ifadenisinin sonucu False olduğu için bu bloğu çalıştırmıyor
+
+number = 10
+if number == 10:
+    print("number is 10")
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+
+number_check(10)
+number_check(20)
+
+#else 
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+    else:
+        print("number is not 10")
+
+number_check(8)
+
+
+#Elif
+
+def number_ten(x):
+    if x > 10:
+        print("Bu sayı 10'dan büyüktür.")
+    elif x < 10: #10'dan büyük değilse(el) ama küçükse(if) "el-if"
+        print("Bu sayı 10'dan küçüktür.")
+    else:
+        print("Bu sayı 10'a eşittir.")
+
+number_ten(50)
+
+# Döngüler (Loops)
+# for loop
+
+students = ["Zehra", "Zeynep", "Melek", "Ahmet"]
+
+# her bir elemana erişmek
+students[0]
+students[1]
+students[2]
+students[3]
+
+for student in students: # ilk önce her bir elemanı gezerken (dönerken) bu elemanları bir şey ile temsil etmeni ister sonra döngüyü nerede gerçekleştireceksin.
+    print(student)
+
+#yakaladığımız her bir öğrencinin ismini büyültmek istersek
+
+for student in students:
+    print(student.upper()) # her bir öğrencinin ismini büyütür.
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    print(salary)
+
+# her bir elemana %20 zam uygulansın
+
+for salary in salaries:
+    print(int(salary * 20 / 100 + salary))
+
+for salary in salaries:
+    print(int(salary * 30 / 100 + salary))
+
+# habire zam değerini girmektense fonksiyon yaz
+
+def new_salary(maas, zam):
+    return int(maas * zam / 100 + maas)
+
+new_salary(1500, 10)
+
+for salary in salaries:
+    print(new_salary(salary, 10))
+
+salaries2 = [10700, 25000, 30400, 40300, 50200]
+
+for salary in salaries2:
+    print(new_salary(salary, 10))
+
+for salary in salaries:
+    if salary >= 3000:
+        print(new_salary(salary, 10))
+    else:
+        print(new_salary(salary, 20))
+
+# Uygulama
+
+#Amaç: aşağıdaki şekilde string değiştiren fonksiyon yazmak istiyoruz.
+#before: "hi my name is john and i am learning python"
+#after: "Hi mY NaMe iS JoHn aNd i aM LeArNiNg pYtHoN"
+#çift indekstekiler büyük tek indeksler küçük
+
+
+# 1. çift ve tek indexlerinde gezmek
+# 2. gezme işlemlerinden sonra indexin tek mi çift mi olduğunu sorgulamak
+# 3. çift olanı büyültmek, tek olanı küçültmek
+# 4. yaptığın değişikleri de bir yere kaydetmek
+
+def alternating(string):
+    new_string = "" # yapılan değişiklikleri bir yere kaydetmek
+    #girilen string'in indexlerinde gez. # girilen herhangi bir stringi boyutunu (len) düşünerek bütün elemanlarını gezmek
+    for string_index in range(len(string)):
+        # index çift ise büyük harfe çevir
+        if string_index % 2 == 0: 
+            new_string += string[string_index].upper() # += ifadesi buraya(new_string) ilgili ifadeyi ekle ve bu ifadenin yeni değeri olarak mevcut hali koru
+        # index tek ise küçük harfe çevir
+        else:
+            new_string += string[string_index].lower() #"zehra"[1].lower()
+    print(new_string)
+
+alternating("zehra")
+
+
+# range(len("zehra")) çıktı --> range(0, 5)
+# range(0, 10) #iki değer arasında sayı üretmeyi sağlar
+for i in range(0, 5):
+    print(i)
+
+for i in range(len("zehra")):
+    print(i)
+
+4 % 2 == 0
+
+m = "zehra"
+m[0]
+
+#break & continue & while
+
